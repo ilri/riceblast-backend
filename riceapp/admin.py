7ad296622ad6efe41e38e2159895b07c5a670463
sokkyyy <ndegwaraymond@gmail.com>
2020-03-05 15:46:19 +0300

    Upload files to update specific db tables.

--- riceapp/admin.py
+++ riceapp/admin.py
@@ -1,15 +1,72 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin,ImportMixin
+from import_export.forms import ImportForm, ConfirmImportForm
+from import_export.widgets import ForeignKeyWidget
+from django import forms
 
-# class RiceBlastAdmin(admin.AdminSite):
-    # site_header = 'Rice Blast'
+
+# Register your models here.
+class RiceGeneImportForm(ImportForm):
+    donor_line = forms.ModelChoiceField(
+        queryset=RiceGenotype.objects.all(),
+        required=True
+    )
+class RiceGeneConfirmImportForm(ConfirmImportForm):
+    donor_line = forms.ModelChoiceField(
+        queryset=RiceGenotype.objects.all(),
+        required=True
+    )
+class RiceGeneResource(resources.ModelResource):
+
+    class Meta:
+        model = RiceGene
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request',None)
+        super(RiceGeneResource, self).__init__(*args, **kwargs)
+    
+    def before_import(self,dataset, using_transactions, dry_run, **kwargs):
+        for i,header in enumerate(dataset.headers):
+            dataset.headers[i] = header.lower()
+    
+    def before_import_row(self,row,**kwargs):
+        donor_line = self.request.POST.get('donor_line')
+        row['donor_line'] = donor_line 
+
+
+
+
+
+class RiceGeneAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = RiceGeneResource
+
+    def get_import_form(self):
+        return RiceGeneImportForm
+    def get_confirm_import_form(self):
+        return RiceGeneConfirmImportForm
+    
+    def get_form_kwargs(self,form,*args, **kwargs):
+        # pass on `donor_line` to the kwargs for the custom confirm form
+        # print(isinstance(form, ImportForm))
+        # print(form)
+        if isinstance(form, RiceGeneImportForm):
+            print(form.is_valid())
+            if form.is_valid():
+                donor_line = form.cleaned_data['donor_line']
+                kwargs.update({'donor_line':donor_line})
+        return kwargs
+
+    def get_resource_kwargs(self,request,*args, **kwargs):
+        return {'request':request}
+    
 
 admin.site.site_header = 'Rice Blast'
 
 admin.site.register(RiceBlastLab)
 admin.site.register(People)
-admin.site.register(Field)
+admin.site.register(FungalCollectionSite)
 admin.site.register(Isolate)
 admin.site.register(RiceGenotype)
 admin.site.register(PathotypingResults)
@@ -19,11 +76,12 @@ admin.site.register(VCGTestResults)
 admin.site.register(Protocol)
 admin.site.register(RiceGBS)
 
-admin.site.register(RiceGene)
+admin.site.register(RiceGene, RiceGeneAdmin)
 admin.site.register(RiceGeneScreenResult)
 admin.site.register(RiceSmallDnaFragmentsSequence)
 admin.site.register(FungalGBS)
-
+admin.site.register(FungalGeneScreenResult)
+admin.site.register(Project)
 
 
 
--- riceapp/models.py
+++ riceapp/models.py
@@ -28,9 +28,17 @@ class People(models.Model):
     def __str__(self):
         return self.full_name
 
+class Project(models.Model):
+    project_name = models.CharField(max_length=255)
+    donor = models.CharField(max_length=255)
+    start = models.DateField()
+    finish = models.DateField()
+    prinicipal_investigator = models.ForeignKey(People, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.project_name
 
-class Field(models.Model):
+class FungalCollectionSite(models.Model):
     ''' Model class for Collection Fields '''
 
     name = models.CharField(max_length=200)
@@ -39,6 +47,7 @@ class Field(models.Model):
     country = CountryField()
     latitude = models.DecimalField(default=0.0000,max_digits=6, decimal_places=4)
     longitude = models.DecimalField(default=0.0000,max_digits=6, decimal_places=4)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
@@ -51,7 +60,7 @@ class Isolate(models.Model):
     date_collected = models.DateField() #!!!
     date_isolated = models.DateField() #!!!!
     country = CountryField()
-    field = models.ForeignKey(Field,on_delete=models.CASCADE)
+    collection_site = models.ForeignKey(FungalCollectionSite,on_delete=models.CASCADE)
     person = models.ForeignKey(People, on_delete=models.CASCADE)
     tissue_type = models.CharField(max_length=100)
 
@@ -85,24 +94,35 @@ class RiceGene(models.Model):
     name = models.CharField(max_length=100)
     chromosome_id = models.IntegerField()
     marker = models.CharField(max_length=100)
-    rice_genotype = models.ForeignKey(RiceGenotype, on_delete=models.CASCADE)
+    donor_line = models.ForeignKey(RiceGenotype, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class RiceGeneScreenResult(models.Model):
+    rice_genotype = models.ForeignKey(RiceGenotype, on_delete=models.CASCADE)
     rice_gene = models.ForeignKey(RiceGene, on_delete=models.CASCADE)
-    pcr_results = models.BooleanField() #POSITIVE OR NEGATIVE
+    pcr_results = models.CharField(max_length=100) #POSITIVE OR NEGATIVE
     replicate_id = models.CharField(max_length=20) #1,2,3
     sample_id = models.CharField(max_length=100)
 
     def __str__(self):
         return self.sample_id
 
+class FungalGeneScreenResult(models.Model):
+    fungal_gene = models.CharField(max_length=255)
+    rice_genotype = models.ForeignKey(RiceGenotype,on_delete=models.CASCADE) 
+    pcr_results = models.CharField(max_length=100) #POSITIVE OR NEGATIVE
+    replicate_id = models.CharField(max_length=20) #1,2,3
+    sample_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.fungal_gene
+
 
 class PathotypingResults(models.Model):
     replicate_id = models.CharField(max_length=100)
-    sample_id = models.CharField(max_length=100)
+    sample_id = models.CharField(max_length=100, unique=True)
     rice_genotype = models.ForeignKey(RiceGenotype, on_delete=models.CASCADE)
     isolate = models.ForeignKey(Isolate, on_delete=models.CASCADE)
     stock_id = models.CharField(max_length=100)
@@ -113,6 +133,7 @@ class PathotypingResults(models.Model):
     lab = models.ForeignKey(RiceBlastLab, on_delete=models.CASCADE)
     disease_score = models.IntegerField()
     test_id = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.sample_id
@@ -129,9 +150,9 @@ class VcgGroup(models.Model):
 class FungalSmallDnaFragmentsSequence(models.Model):
     isolate = models.ForeignKey(Isolate, on_delete=models.CASCADE)
     taxa_name = models.CharField(max_length=200)
-    sequence_id = models.CharField(max_length=100) #unique or not?
+    sequence_id = models.CharField(max_length=100, unique=True) #unique or not?
     description = models.TextField()
-    sequence_data = models.CharField(max_length=200) #UPLOAD A FASTA FILE
+    sequence_data = models.FileField(upload_to='fungal_sequence_data/') #UPLOAD A FASTA FILE
     chromosome_id = models.IntegerField()
     chromosome_site_id = models.CharField(max_length=100)
     loci_id = models.CharField(max_length=100)
@@ -145,9 +166,9 @@ class FungalSmallDnaFragmentsSequence(models.Model):
 class RiceSmallDnaFragmentsSequence(models.Model):
     rice_genotype = models.ForeignKey(RiceGenotype, on_delete=models.CASCADE)
     taxa_name = models.CharField(max_length=200)
-    sequence_id = models.CharField(max_length=100) #unique or not?
+    sequence_id = models.CharField(max_length=100, unique=True) #unique or not?
     description = models.TextField()
-    sequence_data = models.CharField(max_length=200) #UPLOAD A FASTA FILE
+    sequence_data = models.FileField(upload_to='rice_sequence_data/') #UPLOAD A FASTA FILE
     chromosome_id = models.IntegerField()
     chromosome_site_id = models.CharField(max_length=100)
     loci_id = models.CharField(max_length=100)
@@ -167,6 +188,7 @@ class VCGTestResults(models.Model):
     lab = models.ForeignKey(RiceBlastLab, on_delete=models.CASCADE)
     vcg_replicate_id = models.CharField(max_length=100)
     vcg = models.ForeignKey(VcgGroup, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.vcg_test_id 
@@ -175,12 +197,12 @@ class Protocol(models.Model):
     name = models.CharField(max_length=200)
     protocol_id = models.CharField(max_length=100, unique=True)
     key_reference = models.CharField(max_length=500)
-    protocol = models.CharField(max_length=200) #UPLOAD FILE
+    protocol = models.FileField(upload_to='protocols/') #UPLOAD FILE
     person = models.ForeignKey(People, on_delete=models.CASCADE)
     lab = models.ForeignKey(RiceBlastLab, on_delete=models.CASCADE)
     protocol_modified = models.DateField()
     related_protocols = models.CharField(max_length=200)
-    country = CountryField(multiple=True)
+    # country = CountryField(multiple=True)
 
     def __str__(self):
         return self.protocol_id
@@ -190,7 +212,7 @@ class RiceGBS(models.Model):
     rice_gbs_name = models.CharField(max_length=200)
     person = models.ForeignKey(People,on_delete=models.CASCADE)
     lab = models.ForeignKey(RiceBlastLab, on_delete=models.CASCADE)
-    gbs_dataset = models.CharField(max_length=200) #UPLOAD VCF/HAPMAP FILE
+    gbs_dataset = models.FileField(upload_to='rice_gbs_dataset/') #UPLOAD VCF/HAPMAP FILE
 
     def __str__(self):
         return self.rice_gbs_name
@@ -199,7 +221,7 @@ class FungalGBS(models.Model):
     fungal_gbs_name = models.CharField(max_length=200)
     person = models.ForeignKey(People,on_delete=models.CASCADE)
     lab = models.ForeignKey(RiceBlastLab, on_delete=models.CASCADE)
-    gbs_dataset = models.CharField(max_length=200) #UPLOAD VCF/HAPMAP FILE
+    gbs_dataset = models.FileField(upload_to='fungal_gbs_dataset') #UPLOAD VCF/HAPMAP FILE
 
     def __str__(self):
         return self.fungal_gbs_name
