f5f6a680dd2ef197e42ca14abe760cce423bdf9a
sokkyyy <ndegwaraymond@gmail.com>
2020-03-20 11:55:24 +0300

    CORS Whitelist for Deployed front-end.

--- riceblast/settings.py
+++ riceblast/settings.py
@@ -77,7 +77,8 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
+    # 'http://localhost:3000',
+    'https://riceblast-ilri.netlify.com'
 )
 
 
