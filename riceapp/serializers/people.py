de65d670de5ffab50be4bcf5a57777726ab41889
sokkyyy <ndegwaraymond@gmail.com>
2020-04-03 15:54:00 +0300

    People.

--- riceapp/serializers/people.py
+++ riceapp/serializers/people.py
@@ -11,14 +11,17 @@ class PeopleSerializer(serializers.ModelSerializer):
 
 class PeopleUserSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-    
+    lab = serializers.SerializerMethodField()
 
     def get_user(self,person):
         return {
             "email":person.user.email,
             "username":person.user.username,
+            "is_active":person.user.is_active,
         }
-
+    def get_lab(self,person):
+        if person.lab is not None: 
+            return person.lab.lab_name
     class Meta:
         model = People
         fields = ['user','full_name','telephone_number','designation','lab']
